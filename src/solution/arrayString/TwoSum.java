20f11b598d78216aab3a47abe90fecc8778e7842
yusong-shen <yusongtju@gmail.com>
2015-12-23 11:31:31 -0600

    Two Sum 2, when the input array is sorted, use binary search.

--- src/solution/arrayString/TwoSum.java
+++ src/solution/arrayString/TwoSum.java
@@ -20,14 +20,56 @@ public class TwoSum {
         
     }
 
+    /**
+     * when the input array is sorted
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+    	for (int i =0; i<nums.length; i++){
+    		int x = nums[i];
+    		int ind = search(nums, target-x, i+1);
+    		if (ind!=-1)
+    			return new int[] {i+1, ind+1};
+    			
+    	}
+    	throw new IllegalArgumentException("No two sum solution");
+    	
+        
+    }
+    
+    /**
+     * Binary search
+     * @param nums sorted array
+     * @param target
+     * @return
+     */
+	public static int search(int[] nums, int target, int start) {
+		int l = start;
+		int h = nums.length-1;
+		while(l<=h){
+			int m = l+(h-l)/2;
+			if (nums[m]==target) return m;
+			if (nums[m]<target) l=m+1;
+			if (nums[m]>target) h=m-1;
+		}
+		return -1;		
+	}
+
 	public static void main(String[] args) {
 //		int[] numbers={2, 7, 11, 15};
 //		int target=9;
 //		int[] numbers = {-1,-2,-3,-4,-5};
 //	    int target=-8;
 		int[] numbers = {0,4,3,0};
-	    int target=7;
-		System.out.println(Arrays.toString(twoSum(numbers, target)));
+	    int target=0;
+	    System.out.println(search(numbers, target, 1));
+		System.out.println(Arrays.toString(twoSum2(numbers, target)));
+		int[] nums1 = {2, 7, 11, 15, 17, 19};
+		int t = 2;
+		System.out.println(search(nums1, t, 0));
+		
 	}
 
 }
