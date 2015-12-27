1fe874773435bf355f2d9ce7c226c1a613c9fa6e
yusong-shen <yusongtju@gmail.com>
2015-12-27 11:18:22 -0600

    Solve #98 validate a BST by in-order traversal

---
+++ src/solution/tree/SerializeTree297.java
@@ -0,0 +1,25 @@
+package solution.tree;
+
+import library.TreeNode;
+
+public class SerializeTree297 {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+		return null;
+        
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+		return null;
+        
+    }
+
+	public static void main(String[] args) {
+		// Your Codec object will be instantiated and called as such:
+		// SerializeTree codec = new SerializeTree();
+		// codec.deserialize(SerializeTree.serialize(root));
+	}
+
+}
--- src/solution/tree/ValidBST98.java
+++ src/solution/tree/ValidBST98.java
@@ -4,9 +4,39 @@ import library.TreeNode;
 
 public class ValidBST98 {
 
+	private TreeNode prev;
+	
+	/**
+	 * In order traversal
+	 * Left subtree -> current node -> right subtree
+	 * @param root
+	 * @return
+	 */
     public boolean isValidBST(TreeNode root) {
-		return false;
+    	TreeNode prev = null;
+		return isBSTInOrder(root);
         
+    }
+
+    /**
+     * helper function
+     * @param root
+     * @param prev
+     * @return
+     */
+    public boolean isBSTInOrder(TreeNode root){
+    	// empty tree - base case
+    	if (root==null) return true;
+    	// first check the left subtree
+    	if (isBSTInOrder(root.left)){
+    		// then check if current node's value is greater than previous node
+    		// if not, return false
+    		if (prev!=null && root.val<=prev.val) return false;
+    		prev = root;
+    		// finally check the right subtree
+    		return isBSTInOrder(root.right);
+    	}
+    	return false;
     }
 
 	public static void main(String[] args) {
