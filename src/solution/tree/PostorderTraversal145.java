ffdef9af22d814442105ebeff0dd3d6d833a2728
yusong-shen <yusongtju@gmail.com>
2016-01-01 12:18:36 -0600

    Solve #145 post-order traversal by iteration.

--- src/solution/tree/PostorderTraversal145.java
+++ src/solution/tree/PostorderTraversal145.java
@@ -2,6 +2,7 @@ package solution.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import library.TreeNode;
 
@@ -26,6 +27,37 @@ public class PostorderTraversal145 {
     	// finally, root node
     	list.add(root.val);
     }
+
+    /**
+     * Using stack to implement post-order traversal without recursion
+     * @param root
+     * @return
+     */	
+    public List<Integer> IterativePostorderTraversal(TreeNode node) {
+    	List<Integer> list = new ArrayList<Integer>();
+    	Stack<TreeNode> stack = new Stack<>();
+    	TreeNode lastVisited = null;
+    	while(!stack.isEmpty() || node!=null){
+    		if (node!=null){
+    			stack.push(node);
+    			node = node.left;
+    		} else{
+    			TreeNode peek = stack.peek();
+    			// if right exist AND traversing node from left child,
+    			// move right
+    			if (peek.right!=null && lastVisited!=peek.right){
+    				node = peek.right;
+    			}
+    			else{
+    				list.add(peek.val);
+    				lastVisited = stack.pop();
+    			}
+    		}
+    	}
+		return list;
+        
+    }
+    
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
