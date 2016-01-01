da971dd63b21d294fe6a368bac908d0f93b4ded2
yusong-shen <yusongtju@gmail.com>
2016-01-01 11:59:53 -0600

    Solve #144 preorder traversal by iteration

--- src/solution/tree/PreorderTraversal144.java
+++ src/solution/tree/PreorderTraversal144.java
@@ -2,6 +2,7 @@ package solution.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import library.TreeNode;
 
@@ -26,6 +27,34 @@ public class PreorderTraversal144 {
 		preorderHelper(root.right);
 
     }
+    
+    /**
+     * Using stack to implement preorder traversal without recursion
+     * @param root
+     * @return
+     */
+    public List<Integer> IterativePreorderTraversal(TreeNode node) {
+    	List<Integer> list = new ArrayList<Integer>();
+    	Stack<TreeNode> stack = new Stack<>();
+    	while(!stack.isEmpty() || node!=null){
+    		// first, visit the current node
+    		if (node!=null){
+    			list.add(node.val);
+    			// push its right child into stack
+    			if (node.right!=null){
+    				stack.push(node.right);
+    			}
+    			// second, go to the left subtree
+    			node = node.left;
+    		} else{
+    			// if node is null
+    			node = stack.pop();
+    		}
+    	}
+    	
+		return list;
+        
+    }    
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
