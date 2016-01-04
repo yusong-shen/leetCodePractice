1e8e517bec2db6ae6408e3e8b0c59ea587309d88
yusong-shen <yusongtju@gmail.com>
2016-01-04 14:13:50 -0600

    #161 more consice code.

--- src/solution/arrayString/OneEditDistance161.java
+++ src/solution/arrayString/OneEditDistance161.java
@@ -2,7 +2,8 @@ package solution.arrayString;
 
 public class OneEditDistance161 {
 
-    public boolean isOneEditDistance(String s, String t) {
+/*    
+ * public boolean isOneEditDistance(String s, String t) {
     	if (Math.abs(s.length()-t.length())>1) return false;
     	// check modify operation
     	int count = 0;
@@ -38,7 +39,24 @@ public class OneEditDistance161 {
     	}
 		return true;
     }
-
+    */
+	public boolean isOneEditDistance(String s, String t) {
+		for (int i = 0; i < Math.min(s.length(), t.length()); i++) { 
+	        if (s.charAt(i) != t.charAt(i)) {
+	        	// s has the same length as t, 
+	        	// so the only possibility is replacing one char in s and t
+	            if (s.length() == t.length()) 
+	                return s.substring(i + 1).equals(t.substring(i + 1));
+	            // t is longer than s, so the only possibility is deleting one char from t
+	            else if (s.length() < t.length()) 
+	                return s.substring(i).equals(t.substring(i + 1));               
+	            else // s is longer than t, so the only possibility is deleting one char from s
+	                return t.substring(i).equals(s.substring(i + 1));
+	        }
+	    }
+		return Math.abs(s.length()-t.length())==1;   
+       
+	}
 	public static void main(String[] args) {
 		OneEditDistance161 test = new OneEditDistance161();
 		String s = "teacher";
