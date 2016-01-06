70f4f18aa6113b0ec9142f68545e9b78686acab9
yusong-shen <yusongtju@gmail.com>
2016-01-05 20:54:44 -0600

    Add #35 Search Insert Position in Binary Search Category.

---
+++ src/graph/CountComponent323.java
@@ -0,0 +1,15 @@
+package graph;
+
+public class CountComponent323 {
+
+    public int countComponents(int n, int[][] edges) {
+		return n;
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/solution/arrayString/SearchInsertPosition35.java
@@ -0,0 +1,20 @@
+package solution.arrayString;
+
+/**
+ * https://leetcode.com/problems/search-insert-position/
+ * @author yusong
+ *
+ */
+public class SearchInsertPosition35 {
+
+    public int searchInsert(int[] nums, int target) {
+		return target;
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
