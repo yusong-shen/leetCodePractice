4cdc552a394dd6aac9569738bad6c65f9c03f319
yusong-shen <yusongtju@gmail.com>
2016-01-06 16:29:19 -0600

    Solve #280 wiggle sort by using system sort.

---
+++ src/solution/sorting/WiggleSort280.java
@@ -0,0 +1,14 @@
+package solution.sorting;
+
+public class WiggleSort280 {
+
+    public void wiggleSort(int[] nums) {
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
