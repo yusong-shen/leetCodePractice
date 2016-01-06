38caf0cb0f23bc8eb87782fe9c260098b7202a15
yusong-shen <yusongtju@gmail.com>
2016-01-06 14:22:44 -0600

    Solve #153 Fin minimal in rotated sorted array

---
+++ src/solution/binarySearch/FindMinInRotatedArray153.java
@@ -0,0 +1,34 @@
+package solution.binarySearch;
+
+public class FindMinInRotatedArray153 {
+
+    public int findMin(int[] nums) {
+		return binarySearch(nums, 0, nums.length-1);
+        
+    }
+    
+    private int binarySearch(int[] nums, int left, int right){
+    	if (left==right) return nums[left];
+    	if (right-left==1) return Math.min(nums[left], nums[right]);
+    	// sorted
+    	if (nums[left]<nums[right]) return nums[left];
+    	int mid = left + (right - left)/2;
+    	
+    	// go right hand side
+    	if (nums[mid] < nums[left]){
+    		return binarySearch(nums, left, mid);
+    	}
+    	// go left hand side
+    	else {
+    		return binarySearch(nums, mid, right);
+    	} 
+    	
+    }
+
+	public static void main(String[] args) {
+		int[] nums = {4, 5, 6, 7, 0, 1, 2}; // expected 0
+		FindMinInRotatedArray153 test = new FindMinInRotatedArray153();
+		System.out.println(test.findMin(nums)); 
+	}
+
+}
