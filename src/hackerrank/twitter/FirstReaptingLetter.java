d4b7c005d2a52cd2e38edff9ff7c794fdef4067e
yusong-shen <yusongtju@gmail.com>
2016-03-19 13:35:45 -0500

    Solve finding the first repeating letter.

--- src/hackerrank/twitter/FirstReaptingLetter.java
+++ src/hackerrank/twitter/FirstReaptingLetter.java
@@ -1,14 +1,37 @@
 package hackerrank.twitter;
 
+import java.util.Arrays;
+
 public class FirstReaptingLetter {
 
-	public static String solution(String s) {
-		return "";
+	public static int solution(String s) {
+		int[] map = new int[256];
+		Arrays.fill(map, -1);
+		int min = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < s.length(); i++){
+			char ch = s.charAt(i);
+//			System.out.println(min);
+			
+			if (map[ch] == -1){
+				map[ch] = i;
+			} 
+			// encounter repeat letter
+			else{
+				min = Math.min(map[ch], min);
+			}
+		}
+		
+		
+		return 	(min == Integer.MAX_VALUE) ? -1 : min;
+
 	}
 
 	public static void main(String[] args) {
 		String s1 = "abcba";
-		System.out.println("a" == solution(s1));
+		System.out.println(solution(s1));
+		System.out.println(0 == solution(s1));
+		
 	}
 
 }
