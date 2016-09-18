b817d8c00833c0221176f584db79aa191e79d2e7
yusong-shen <yusongtju@gmail.com>
2016-09-18 16:22:09 -0500

    apply git ignore

---
+++ src/coursera/LovelyNumber.java
@@ -0,0 +1,14 @@
+package coursera;
+
+public class LovelyNumber {
+
+	public LovelyNumber() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/coursera/RoyalNumber.java
@@ -0,0 +1,23 @@
+package coursera;
+
+import java.util.Arrays;
+
+public class RoyalNumber {
+
+	public RoyalNumber() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String[] getSortedList(String[] names) {
+		
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names = {"Louis IX", "Louis VIII"};
+		String[] expected = {"Louis VIII", "Louis IX"};
+		System.out.println(Arrays.deepEquals(getSortedList(names), expected));
+	}
+
+}
